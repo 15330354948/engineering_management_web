823ca4f22a7f97e8f96d22bb98a8c391741e5493
luckept <luckept@qq.com>
2021-09-23 17:49:50 +0800

    fix some bugs

--- src/views/maintenance/mtask/index.vue
+++ src/views/maintenance/mtask/index.vue
@@ -9,21 +9,43 @@
           label-width="200px"
           class="mtaskForm"
         >
-          <el-form-item prop="projectName">
+          <el-form-item prop="mtaskName">
             <el-input
               type="text"
-              placeholder="请输入项目名称"
-              v-model="mtaskForm.projectName"
+              placeholder="请输入维护任务名称"
+              v-model="mtaskForm.mtaskName"
               autocomplete="off"
             ></el-input>
           </el-form-item>
-          <el-form-item label-width="200px" prop="checkPerson">
+          <el-form-item prop="mtaskPerson">
+            <el-input
+              type="text"
+              placeholder="请输入维护人"
+              v-model="mtaskForm.mtaskPerson"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="planningDate">
+            <el-date-picker
+            v-model="mtaskForm.planningDate"
+            type="date"
+            placeholder="请选择计划时间">
+          </el-date-picker>
+          </el-form-item>
+          <el-form-item prop="finishDate">
+            <el-date-picker
+            v-model="mtaskForm.finishDate"
+            type="date"
+            placeholder="请选择实际完成时间">
+          </el-date-picker>
+          </el-form-item>
+          <el-form-item label-width="200px" prop="mtaskStatus">
             <el-select
-              v-model="mtaskForm.checkPerson"
-              placeholder="请选择巡检人"
+              v-model="mtaskForm.mtaskStatus"
+              placeholder="请选择状态"
             >
               <el-option
-                v-for="item in checkPersonList"
+                v-for="item in mtaskStatusList"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
@@ -31,20 +53,24 @@
               </el-option>
             </el-select>
           </el-form-item>
-          <el-form-item prop="dateInfo">
-            <el-date-picker
-              v-model="mtaskForm.dateInfo"
-              type="datetimerange"
-              range-separator="至"
-              start-placeholder="开始日期"
-              end-placeholder="结束日期"
+          <el-form-item label-width="200px" prop="mtaskResult">
+            <el-select
+              v-model="mtaskForm.mtaskStatus"
+              placeholder="请选择结果"
             >
-            </el-date-picker>
+              <el-option
+                v-for="item in mtaskResultList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
           </el-form-item>
         </el-form>
       </div>
       <!-- 使用该组件获取点击通知 -->
-      <TableSearch :pageSign="pageSign"></TableSearch>
+      <div style="position:absolute; right: 10px"><TableSearch :pageSign="pageSign"></TableSearch></div>
     </div>
 
     <!-- 表格 -->
@@ -101,22 +127,61 @@ export default {
     return {
       pageSign: "mtask",
       mtaskForm: {
-        // 项目名称
-        projectName: "",
-        // 巡检人
-        checkPerson: "",
-        // 日期
+        // 维护任务名称
+        mtaskName: "",
+        // 维护人
+        mtaskPerson: "",
+        // 计划时间
+        planningDate: "",
+        // 实际完成时间
+        finishDate: "",
         dateInfo: "",
+        // 状态
+        mtaskStatus: "",
+        // 结果
+        mtaskResult: ""
       },
-      checkPersonList: [
+      // 状态列表
+      mtaskStatusList: [
         {
-          label: "测试人1",
-          value: "testPerson1",
+          label: "待维护",
+          value: 0,
         },
         {
-          label: "测试人2",
-          value: "testPerson2",
+          label: "进行中",
+          value: 1,
         },
+        {
+          label: "待审核",
+          value: 2,
+        },
+        {
+          label: "已审核",
+          value: 3,
+        },
+        {
+          label: "已逾期",
+          value: 4,
+        },
+      ],
+      // 结果列表
+      mtaskResultList: [
+        {
+          label: "优秀",
+          value: 0
+        },
+        {
+          label: "良好",
+          value: 1
+        },
+        {
+          label: "一般",
+          value: 2
+        },
+        {
+          label: "较差",
+          value: 3
+        }
       ],
       // 表格数据
       tableData: [
